077e7d9030422b326fccf0b9fd64fa8e23fc4dff
acsamendes <acsasantos@discente.ufg.br>
2025-12-01 21:24:52 -0300

    [FIX] Ajustes em IaC para teste de execução

--- src/main.py
+++ src/main.py
@@ -145,4 +145,4 @@ def resolve_dependencies(request: ResolveRequest, db_client: DBClient = Depends(
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
